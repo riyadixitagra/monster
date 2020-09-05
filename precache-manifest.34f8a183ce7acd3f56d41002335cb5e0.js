e0b506371e5804c28b287da6c8fb755f638776e0
riyadixitagra <riyadixitagra@gmail.com>
2020-09-06 00:58:37 +0530

    Updates

---
+++ precache-manifest.34f8a183ce7acd3f56d41002335cb5e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3fc3ab2708a1fe9b0df6000981348a5",
+    "url": "/monster/index.html"
+  },
+  {
+    "revision": "d46d531f2fca2429a08b",
+    "url": "/monster/static/css/main.f5836710.chunk.css"
+  },
+  {
+    "revision": "7df446286706808dca18",
+    "url": "/monster/static/js/2.0c171e74.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster/static/js/2.0c171e74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d46d531f2fca2429a08b",
+    "url": "/monster/static/js/main.74e842fb.chunk.js"
+  },
+  {
+    "revision": "9633b500db4096510c8c",
+    "url": "/monster/static/js/runtime-main.9e9c0241.js"
+  }
+]);
